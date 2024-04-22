2714318c3e63eae7ee43bb92f0390e2aa62bc356
Kishor <124120029+moltresIn@users.noreply.github.com>
2024-04-22 07:12:00 +0000

    Uploading the files

---
+++ assets/Javascrpit.js
@@ -0,0 +1,17 @@
+const result = document.getElementById("number");
+
+function appendToDisplay(input) {
+  result.value += input;
+}
+
+function clearDisplay() {
+  result.value = "";
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    result.value = "Error";
+  }
+}
